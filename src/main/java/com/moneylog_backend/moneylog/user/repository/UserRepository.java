c42d20260e231bd20c8a00ed67a60309db0ef615
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-27 14:56:58 +0900

    feat : 회원가입 service만 완성

--- src/main/java/com/moneylog_backend/moneylog/user/repository/UserRepository.java
+++ src/main/java/com/moneylog_backend/moneylog/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<UserEntity, Integer> {
 
     UserEntity findById(String id);
+
+    boolean existsByEmail(String email);
 } // interface end
--- src/main/java/com/moneylog_backend/moneylog/user/service/UserService.java
+++ src/main/java/com/moneylog_backend/moneylog/user/service/UserService.java
@@ -1,17 +1,45 @@
 package com.moneylog_backend.moneylog.user.service;
 
+import com.moneylog_backend.global.auth.jwt.JwtProvider;
+import com.moneylog_backend.global.util.RedisService;
+import com.moneylog_backend.moneylog.user.dto.UserDto;
+import com.moneylog_backend.moneylog.user.entity.UserEntity;
 import com.moneylog_backend.moneylog.user.mapper.UserMapper;
 import com.moneylog_backend.moneylog.user.repository.UserRepository;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final RedisService redisService;
+    private final JwtProvider jwtProvider;
     private final UserMapper userMapper;
 
+    @Transactional
+    public int signup(UserDto userDto){
+        if (userRepository.existsByEmail(userDto.getEmail())){
+            throw new RuntimeException("이미 가입된 이메일입니다.");
+        } // if end
 
+        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+        UserEntity userEntity = UserEntity.builder()
+                .name(userDto.getName())
+                .password(encodedPassword)
+                .email(userDto.getEmail())
+                .phone(userDto.getPhone() == null ? "" : userDto.getPhone())
+                .gender(userDto.isGender())
+                .role(userDto.getRole())
+                .profile_image_url(userDto.getProfile_image_url() == null ? "" : userDto.getProfile_image_url())
+                .build();
+        return userRepository.save(userEntity).getUser_id();
+    } // func end
 } // class end
