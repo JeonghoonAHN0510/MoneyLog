c2945fdacebf37b731eea54998c0cbdb17e2232f
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-17 22:28:59 +0900

    feat : 회원별 계좌목록 조회 기능 구현

--- src/main/java/com/moneylog_backend/moneylog/account/mapper/AccountMapper.java
+++ src/main/java/com/moneylog_backend/moneylog/account/mapper/AccountMapper.java
@@ -1,8 +1,14 @@
 package com.moneylog_backend.moneylog.account.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.moneylog_backend.moneylog.account.dto.AccountDto;
 
 @Mapper
 public interface AccountMapper {
     int checkAccountNumber(String account_number);
+
+    List<AccountDto> getAccountsByUserId(int user_id);
 }
